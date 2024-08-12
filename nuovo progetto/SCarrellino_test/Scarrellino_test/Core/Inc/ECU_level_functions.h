9380bc1ad84329fc1a7fc47a40e6a0deb9341b9f
Simone Ruffini <simone.ruffini.work@gmail.com>
2024-08-12 08:55:02 +0200

    chore: tidy up dirs

--- nuovo progetto/SCarrellino_test/Scarrellino_test/Core/Inc/ECU_level_functions.h
+++
@@ -1,5 +0,0 @@
-#include "stdio.h"
-#include "can.h" 
-
-
-void CAN_ECU_Tx(CAN_HandleTypeDef *hcan, uint8_t Txdata,uint8_t id,uint32_t TxMailbox );
--- nuovo progetto/SCarrellino_test/Scarrellino_test/Core/Inc/can_functions.h
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-
-void can_send_msg(uint32_t id);
-HAL_StatusTypeDef can_send(CAN_HandleTypeDef *hcan, uint8_t *buffer, CAN_TxHeaderTypeDef *header,uint32_t *mailbox);
-HAL_StatusTypeDef can_wait(CAN_HandleTypeDef *hcan, uint8_t timeout);
